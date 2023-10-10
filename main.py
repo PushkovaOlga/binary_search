36157e33c9e44449b14131ba9f10a12701251fdd
TatianaMaksakova <tatzianar@gmail.com>
2023-10-10 14:06:57 +0300

    a

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
 #напишите программу для бинарного поиска загаданного числа
 
+user_num = int(input("Введите число: "))
 def binary_search():
-    pass
+    min_num = 0
+    max_num = 100
+    center = (min_num + max_num) // 2
+
+    while True:
+        print(f"Ваше число {center}?")
+        check = input("Если больше >, если меньше <, если равно =")
+
+        if check == ">":
+           min_num = center
+           center = (min_num + max_num) // 2
+        elif check == "<":
+           max_num = center
+           center = (min_num + max_num) // 2
+        elif check == "=":
+            print(f"Вы загадали {check}")
+            break
+        else:
+            print("Я не знаю этого числа")
+
+
+binary_search()
