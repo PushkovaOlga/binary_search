4d877e8c555995b9084fa7e4352f51129d75519a
RPO23/2 : Karaputa : ex:binary_search <s.karaputa2017@yandex.ru>
2023-10-12 22:12:02 +0300

    Эта функция binary_search принимает отсортированный список arr и целевое число target. Она возвращает индекс элемента в списке, если число найдено, и -1, если число не найдено.

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-#напишите программу для бинарного поиска загаданного числа
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
 
-def binary_search():
-    pass
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+target = 5
+result = binary_search(arr, target)
+if result != -1:
+    print(f"Число {target} найдено {result}.")
+else:
+    print(f"Число {target} не найдено.")
