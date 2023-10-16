5484db587f263e0e56e4d6e037bc4690ddeb44fd
Zzar23 <maksimchibotar@gmail.com>
2023-10-16 18:43:06 +0300

    update: update func binary_search

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 #напишите программу для бинарного поиска загаданного числа
 
-def binary_search():
-    pass
+
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+
+    while left <= right:
+        mid = left + (right - left) // 2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+arr = [1, 3, 5, 7, 9, 11, 13, 15]
+target = 7
+result = binary_search(arr, target)
+
+if result != -1:
+    print(f"Элемент {target} найден в позиции {result}.")
+else:
+    print(f"Элемент {target} не найден в массиве.")
